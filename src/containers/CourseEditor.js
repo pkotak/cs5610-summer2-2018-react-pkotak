7c4fff1e4480f02b060c23c7abe217fac71b1d9e
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-16 17:32:51 -0400

    Course editor created

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import LessonsTab from "./LessonsTab";
+import ModuleList from "./ModuleList";
+
+export default class CourseEditor
+    extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.selectCourse = this.selectCourse.bind(this);
+        this.state = {courseId: ''};
+    }
+
+    selectCourse(courseId){
+        this.setState({courseId: courseId});
+    }
+
+    componentDidMount(){
+        this.selectCourse(this.props.match.params.courseId);
+    }
+
+    componentWillReceiveProps(newProps){
+        this.selectCourse(newProps.match.params.courseId);
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>Course : {this.state.courseId}</h3>
+                <div className='row'>
+                    <div className='col-4'>
+                        <ModuleList/>
+                    </div>
+                    <div className='col-8'>
+                        <LessonsTab/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
