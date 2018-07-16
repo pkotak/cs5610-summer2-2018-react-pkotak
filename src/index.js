fef4224ecfdd1a40fcae5d8fc99cb77c089c20d3
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-16 17:35:24 -0400

    project commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import CourseManager from './containers/CourseManager';
+import '../node_modules/font-awesome/css/font-awesome.css';
+
+ReactDOM.render(
+    <CourseManager/>,
+    document.getElementById('root')
+);
