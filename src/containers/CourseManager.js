d30a1a1b490e2f702326971bf4d2496dbae40ac7
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-16 17:30:43 -0400

    Course manager with routing implemented

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import CourseEditor from './CourseEditor';
+import CourseList from "./CourseList";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+export default class CourseManager
+    extends Component{
+    render(){
+        return (
+                <Router>
+                    <div className="container-fluid">
+                        <h1>Course Manager</h1>
+                        <Route path="/courses" component={CourseList}/>
+                        <Route path="/course/:courseId/edit" component={CourseEditor}/>
+                    </div>
+                </Router>
+        )
+    }
+}
