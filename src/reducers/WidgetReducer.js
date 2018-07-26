bb7bbee22369ffdae56782b118cd37be53c65cda
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-25 21:12:09 -0400

    initial widget reducer created

---
+++ src/reducers/WidgetReducer.js
@@ -0,0 +1,12 @@
+let initialState = {
+    widgets: [
+        {id: 1, title: 'Widget 1'},
+        {id: 2, title: 'Widget 2'},
+        {id: 3, title: 'Widget 3'},
+        {id: 4, title: 'Widget 4'}
+    ]
+}
+
+export const widgetReducer = (state = initialState) => (
+    state
+)
